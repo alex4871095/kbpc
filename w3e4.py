78f795c88c151d52bdd47bee0fa05d3e1f681fa0
Alexey Agaev <alex@Alexeys-MacBook-Pro.local>
2020-11-04 10:41:30 +0300

    Added 'cause some troubles with python3 and sys path

--- w3e4.py
+++ w3e4.py
@@ -1,4 +1,4 @@
-net = raw_input("Enter the network: ")
+net = input("Enter the network: ")
 splitted_net = net.split('.')
 f = 1
 if len(splitted_net) == 4:
@@ -6,23 +6,23 @@ if len(splitted_net) == 4:
 	o2 = int(splitted_net[1])
 	o3 = int(splitted_net[2])
 	o4 = int(splitted_net[3])
-        if o1 > 0 and o1 < 224 and o1 != 127:
+	if o1 > 0 and o1 < 224 and o1 != 127:
 		if o3 >= 0 and o3 <= 255 and o4 >= 0 and o4 <= 255:
-                	if o1 == 169 and o2 == 254:
+			if o1 == 169 and o2 == 254:
 				f = 1
 			else:
-                                f = 0
-                else:
-                        f = 1
-        else:
-                f = 1
+				f = 0
+		else:
+			f = 1
+	else:
+		f = 1
 
 else:
 	f = 1
 
-print net
+print (net)
 if f == 0:
-	print "IP address is valid"
+	print ("IP address is valid")
 else:
-	print "IP address is invalid"
+	print ("IP address is invalid")
 
